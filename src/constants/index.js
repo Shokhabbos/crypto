0b1b5391b9461e6236417d2ed69d9f05b50e2186
shokhabbos <urinovs999@gmail.com>
2023-11-28 22:17:18 +0500

    modified file

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
-const API_KEY =
-  "9213cb766fb502ad31d61e2996dc321513611ba5664b4b51c364be7db764e9c9";
+const API_KEY = "9213cb766fb502ad31d61e2996dc321513611ba5664b4b51c364be7db764e9c9";
 const API_URL = "https://min-api.cryptocompare.com/data/price";
 
 export { API_KEY, API_URL };
